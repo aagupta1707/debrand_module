8d26c7fc0ae6103868198733f5e6f393d91d60ee
ayush.iqminds <ayush@iqminds.org>
2020-04-01 12:09:25 +0530

    add

--- til_custom_module/models/models.py
+++ til_custom_module/models/models.py
@@ -6,12 +6,11 @@ from odoo import models, fields, api
 class StockMoveInherit(models.Model):
     _inherit = 'stock.move'
 
-    carton_no = fields.Char('Cartoon Number')
-    dn_serial_number = fields.Char('DN Number')
+    carton_no = fields.Char('Carton Number')
 
 
 class SaleOrderInherit(models.Model):
     _inherit = 'sale.order'
 
     s_no = fields.Char('Serial No.')
-    project_name = fields.Char('Project Name')
+    project_name = fields.Many2one('project.project','Project Name')
