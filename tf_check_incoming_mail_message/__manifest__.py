293d76ef0094b8a50e90b98ce0fa81e200e44283
ayush.iqminds <ayush@iqminds.org>
2020-04-12 01:40:04 +0530

    add module for Check string in incoming mail

---
+++ tf_check_incoming_mail_message/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Incoming Mail Alert",
+
+    'summary': """
+       Voice Alert When In Subject Emergency is found""",
+
+    'description': """
+        Voice Alert When In Subject Emergency is found in the Incoming mail
+    """,
+
+    'author': "Ayush Gupta",
+    'website': "http://www.theerpstore.com",
+
+    'category': 'Mail',
+    'version': '13.0.1',
+
+    'depends': ['base','fetchmail'],
+}
